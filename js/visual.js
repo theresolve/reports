661d4f3787e15f8bba09871d3ca3df81f40d7bd3
ktransier <ktransier@gmail.com>
2013-08-27 16:04:57 -0700

    work in progress

--- js/visual.js
+++ js/visual.js
@@ -1,68 +1,47 @@
-// Load Google Chart Library
-google.load('visualization', '1.0', {'packages':['corechart']});
-
-// Load visuals
-google.setOnLoadCallback(drawVisuals);
-
-function drawVisuals() {
-  drawVisual1()
-  drawVisual2()
-}
-
-function drawVisual1() {
-
-// Create and populate the data table.
-  var data = google.visualization.arrayToDataTable([
-    ['Month', 'DRC', 'CAR', 'S. Sudan'],
-    ['Apr. 2012',  133,    400,    100],
-    ['June',  1538,    366,    111],
-    ['Aug.',  157,    440,    993],
-    ['Oct.',  160,    434,    1004],
-    ['Dec.',  196,    393,    978],
-    ['Apr. 2012',  130,    400,    100],
-    ['June',  153,    366,    111],
-    ['Aug.',  157,    440,    993],
-    ['Oct.',  160,    434,    1004],
-    ['Dec.',  193,    393,    979],
-    ['Apr. 2012',  133,    400,    1001],
-    ['June',  153,    366,    111],
-    ['Aug.',  157,    440,    993],
-    ['Oct.',  160,    434,    100],
-    ['Dec.',  196,    393,    979]
-  ]);
-
-  // Create and draw the visualization.
-  new google.visualization.ColumnChart(document.getElementById('attack-by-country-visual')).
-      draw(data,
-           {title:"Reported LRA Attacks by Country",
-            width:800, height:400,
-            colors: ["#1D1D1D", "#DBDBDB", "#CB4133"],
-            vAxis: {title: "Year"},
-            hAxis: {title: "LRA Attacks"},
-            isStacked: true}
-      );
-}
-
-function drawVisual2() {
-
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Killings & Abductions', 45],
-    ['Killings Only', 90],
-    ['Abductions Only', 13],
-    ['Other', 4],
-  ]);
-
-  // Set chart options
-  var options = {'title':'Attack Type',
-                 'width':400,
-                 'height':300,
-                 'colors' :["#1D1D1D", "#CB4133", "#DBDBDB"]};
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('trends-visual'));
-  chart.draw(data, options);
-}
+$(function () {
+        $('#attack-by-country-visual').highcharts({
+            title: {
+                text: 'Attacks by Country',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'January 2011 - January 2013',
+                x: -20
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: 'Attacks'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'DR Congo',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'CAR',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            }, {
+                name: 'S Sudan',
+                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            }, {
+                name: 'Sudan',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            }]
+        });
+    });
