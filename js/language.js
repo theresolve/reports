83f044a53fd4084ce4831a3956c0938c3cd0b19f
ktransier <ktransier@gmail.com>
2013-10-28 22:39:24 -0700

    rebuilt for use with multiple reports, started q3

---
+++ js/language.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('#french').click(function() { 
+    if (location.pathname.split('/')[2] && location.pathname.split('/')[3]) {
+      window.location = "/fr/" + location.pathname.split('/')[2] + "/" + location.pathname.split('/')[3];
+    } else {
+      window.location = "/fr/";
+    }
+    return false;
+  });
+});
+
+$(function () {
+  $('#english').click(function() { 
+    if (location.pathname.split('/')[2] && location.pathname.split('/')[3]) {
+      window.location = "/en/" + location.pathname.split('/')[2] + "/" + location.pathname.split('/')[3];
+    } else {
+      window.location = "/";
+    }
+    return false;
+  });
+});
--- js/midyear-2013-visual.js
+++ js/midyear-2013-visual.js
@@ -1,26 +1,4 @@
 $(function () {
-  $('#french').click(function() { 
-    if (location.pathname.split('/')[2] && location.pathname.split('/')[3]) {
-      window.location = "/fr/" + location.pathname.split('/')[2] + "/" + location.pathname.split('/')[3];
-    } else {
-      window.location = "/fr/";
-    }
-    return false;
-  });
-});
-
-$(function () {
-  $('#english').click(function() { 
-    if (location.pathname.split('/')[2] && location.pathname.split('/')[3]) {
-      window.location = "/en/" + location.pathname.split('/')[2] + "/" + location.pathname.split('/')[3];
-    } else {
-      window.location = "/";
-    }
-    return false;
-  });
-});
-
-$(function () {
   if ($('#decline-mass-events-congo').length) {
   $('#decline-mass-events-congo').highcharts({
     chart: {
