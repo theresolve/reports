f8044e6a5dbf8859d891174ae95b10b0b8ec5746
ktransier <ktransier@gmail.com>
2013-11-25 17:23:54 -0800

    additions

--- js/q3-2013-visual.js
+++ js/q3-2013-visual.js
@@ -25,10 +25,12 @@ function getVisualData() {
   $.when(get_executive_summary_1_data, get_executive_summary_2_drc_data, get_executive_summary_2_car_data).done(function() {
     buildExecutiveSummary1(executive_summary_1_data)
     buildExecutiveSummary2(executive_summary_2_drc_data, executive_summary_2_car_data)
+    buildUAG()
   });
 };
 
 function buildExecutiveSummary1(executive_summary_1_data) {
+    if ($('#executive-summary-1').length) {
   $('#executive-summary-1').highcharts({
     chart: {
       type: 'line'
@@ -67,8 +69,10 @@ function buildExecutiveSummary1(executive_summary_1_data) {
     }]
   });
 };
+};
 
 function buildExecutiveSummary2(executive_summary_2_drc_data, executive_summary_2_car_data) {
+  if ($('#executive-summary-2').length) {
   $('#executive-summary-2').highcharts({
     chart: {
         type: 'column'
@@ -114,4 +118,49 @@ function buildExecutiveSummary2(executive_summary_2_drc_data, executive_summary_
         data: executive_summary_2_drc_data
     }]
   });
+ };
+};
+
+function buildUAG() {
+    if ($('#uag-q3-2013').length) {
+      $('#uag-q3-2013').highcharts({
+      chart: {
+          type: 'column'
+      },
+      colors: ["#2f7ed8",'#0d233a', '#525252', '#910000'],
+      title: {
+        text: 'LRA vs. Unidentified Armed Group (UAG) Attacks in LRA-affected Areas of Congo'
+      },
+      subtitle: {
+        text: 'July-Sept 2013'
+      },
+      xAxis: {
+          labels: {
+            enabled: false
+          }
+      },
+      yAxis: {
+          min: 0,
+          title: {
+              text: 'Number of Attacks'
+          }
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+          name: 'LRA',
+          data: [31]
+      }, {
+          name: 'UAG',
+          data: [10]
+      }]
+    });
+  }
 };
