68d33e0f50d158df6dff03ebee019be6d8119370
pauldronan <paul@theresolve.org>
2014-04-28 17:28:02 -0400

    Update q1-2014-visual.js

--- js/q1-2014-visual.js
+++ js/q1-2014-visual.js
@@ -299,8 +299,8 @@ function buildHU() {
     if (language == "fr") {
     } else {
       x_axis_categories = ['Q1 2011', 'Q2', 'Q3', 'Q4', 'Q1 2012', 'Q2', 'Q3', 'Q4', 'Q1 2013', 'Q2', 'Q3', 'Q4', 'Q1 2014'];
-      series_name_1 = '# of LRA attacks in Niangara-Bangadi area, Congo';
-      series_name_2 = '# of LRA abductions in Niangara-Bangadi area, Congo';
+      series_name_1 = '# of LRA attacks in the Niangara-Bangadi area, Congo';
+      series_name_2 = '# of LRA abductions in the Niangara-Bangadi area, Congo';
     };
 
     $('#Q12014_HU').highcharts({
